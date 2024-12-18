49537ed4221af97cf4f7ee4c048ff7c29d712a19
Wandson | Notebook <wandson7emanuel@hotmail.com>
2024-12-18 00:13:49 -0300

    adicionando gráfico

--- src/api/mainAPI.js
+++ src/api/mainAPI.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default async function conection() {
-    try {
-        const response = await axios.get('http://localhost:8082/api/wallets');
-        console.log('Conectado com sucesso!');
-        return response.data;
-    } catch (error) {
-        console.error('Erro na conexão:', error.response?.status || error.message);
-    }
+    // try {
+    //     const response = await axios.get('http://localhost:8082/api/wallets');
+    //     console.log('Conectado com sucesso!');
+    //     return response.data;
+    // } catch (error) {
+    //     console.error('Erro na conexão:', error.response?.status || error.message);
+    // }
 }
